49c8f2fbb25dbdc420328b29c9a2e8f649c3058a
shuoli90 <lishuo1@seas.upenn.edu>
2022-05-16 11:50:24 -0400

    Lower pickle version

--- readPickle.py
+++ readPickle.py
@@ -4,10 +4,13 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--parameters_file", type=str, default="parameters_1.pickle")
+    parser.add_argument("--parameters_file", type=str, default="parameters_2.pickle")
     args = parser.parse_args()
 
     with open(args.parameters_file, 'rb') as handle:
-            parameters = pickle.load(handle)
+        parameters = pickle.load(handle)
+    
+    # with open(args.parameters_file, 'wb') as handle:
+    #     pickle.dump(parameters, handle, protocol=4)
 
     print(parameters)
