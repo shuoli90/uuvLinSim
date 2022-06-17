ef3ee1dfe8016a81ac331c64f2e50b7c20c6cb1a
shuoli90 <lishuo1@seas.upenn.edu>
2022-06-17 10:04:21 -0400

    Update to truncated normal

--- sampling.py
+++ sampling.py
@@ -31,25 +31,25 @@ a = 0.0
 b = 1.0
 
 # a, b = (a - mean) / std, (b - mean) / std
-# cdf1 = truncnorm.cdf(0.0, a, b, loc=mean, scale=std)
-# cdf2 = truncnorm.cdf(0.2, a, b, loc=mean, scale=std)
-# cdf3 = truncnorm.cdf(0.4, a, b, loc=mean, scale=std)
-# cdf4 = truncnorm.cdf(0.6, a, b, loc=mean, scale=std)
-# cdf5 = truncnorm.cdf(0.8, a, b, loc=mean, scale=std)
-# cdf6 = truncnorm.cdf(1.0, a, b, loc=mean, scale=std)
-cdf1 = norm.cdf(0.0, loc=mean, scale=std)
-cdf2 = norm.cdf(0.2, loc=mean, scale=std)
-cdf3 = norm.cdf(0.4, loc=mean, scale=std)
-cdf4 = norm.cdf(0.6, loc=mean, scale=std)
-cdf5 = norm.cdf(0.8, loc=mean, scale=std)
-cdf6 = norm.cdf(1.0, loc=mean, scale=std)
+cdf1 = truncnorm.cdf(0.0, a, b, loc=mean, scale=std)
+cdf2 = truncnorm.cdf(0.2, a, b, loc=mean, scale=std)
+cdf3 = truncnorm.cdf(0.4, a, b, loc=mean, scale=std)
+cdf4 = truncnorm.cdf(0.6, a, b, loc=mean, scale=std)
+cdf5 = truncnorm.cdf(0.8, a, b, loc=mean, scale=std)
+cdf6 = truncnorm.cdf(1.0, a, b, loc=mean, scale=std)
+# cdf1 = norm.cdf(0.0, loc=mean, scale=std)
+# cdf2 = norm.cdf(0.2, loc=mean, scale=std)
+# cdf3 = norm.cdf(0.4, loc=mean, scale=std)
+# cdf4 = norm.cdf(0.6, loc=mean, scale=std)
+# cdf5 = norm.cdf(0.8, loc=mean, scale=std)
+# cdf6 = norm.cdf(1.0, loc=mean, scale=std)
 b1 = cdf2 - cdf1
 b2 = cdf3 - cdf2
 b3 = cdf4 - cdf3
 b4 = cdf5 - cdf4
 b5 = cdf6 - cdf5
 
-weights = norm.pdf(X, loc=mean, scale=std) / 1.0
+weights = truncnorm.pdf(X, a, b, loc=mean, scale=std) / 1.0
 estimated_safety = np.mean(weights * y)
 true_safety = b1 * 0.1 + b2 * 0.3 + b3 * 0.5 + b4 * 0.7 + b5 * 0.9
 sample_safety = np.mean(y)
